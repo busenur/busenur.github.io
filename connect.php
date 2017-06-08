2c171211eb8a5ebcda80713dfb2b32841e5b9c3f
busenur <busenur49@gmail.com>
2017-06-08 10:55:49 +0300

    initial

---
+++ connect.php
@@ -0,0 +1,7 @@
+<?php
+   define('DB_SERVER', 'localhost');
+   define('DB_USERNAME', 'root');
+   define('DB_PASSWORD', 'busenu');
+   define('DB_DATABASE', 'buse');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
